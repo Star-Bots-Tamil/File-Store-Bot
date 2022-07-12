0de2417762eae2d7b9a5bb885f2be57439de7cda
HMTD-Links-2 <109021412+HMTD-Links@users.noreply.github.com>
2022-07-12 07:21:42 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/KarthikTamil7/File-Sharing-Bot'>Click here</a>\n○ Channel : @HMTD_Links\n○ Discussion Group : @HMTD_Discussion_Group</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/HMTD-Links/File-Store-Bot-V2'>Click here</a>\n○ Channel : @HMTD_Links\n○ Discussion Group : @HMTD_Discussion_Group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
